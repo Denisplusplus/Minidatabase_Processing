0e8ae0a511f81f9b885acb4874b002c36fc7ab70
Denisplusplus <whinslow@mail.ru>
2017-06-14 18:12:03 +0300

    Update read_write.c

--- Code/read_write.c
+++ Code/read_write.c
@@ -19,12 +19,12 @@ int passenger_read_txt(Passenger *p, FILE *in)
 
 void passenger_write_bin(Passenger *p, FILE *out)
 {
-    fwrite(p->surname,  sizeof(char), SIZE_SURNAME, out);
-    fwrite(p->initials, sizeof(char), SIZE_INITIALS, out);
+    fwrite(p->surname,  sizeof(char), SIZE_STR, out);
+    fwrite(p->initials, sizeof(char), SIZE_STR, out);
     fwrite(&(p->baggage_quantity), sizeof(int), 1, out);
-    fwrite(&(p->baggage_weight), sizeof(double), 1, out);
-    fwrite(p->destination, sizeof(char), SIZE_DESTINATION, out);
-    fwrite(p->time, sizeof(char), SIZE_TIME, out);
+    fwrite(&(p->baggage_weight), sizeof(int), 1, out);
+    fwrite(p->destination, sizeof(char), SIZE_STR, out);
+    fwrite(p->time, sizeof(char), SIZE_STR, out);
     fwrite(&(p->transplants), sizeof(char), 1, out);
     fwrite(&(p->children), sizeof(char), 1, out);
 
@@ -32,15 +32,15 @@ void passenger_write_bin(Passenger *p, FILE *out)
 
 int passenger_read_bin(Passenger *p, FILE *in)
 {
-    fread(p->surname,  sizeof(char), SIZE_SURNAME, in);
-    fread(p->initials, sizeof(char), SIZE_INITIALS, in);
+    fread(p->surname,  sizeof(char), SIZE_STR, in);
+    fread(p->initials, sizeof(char), SIZE_STR, in);
     fread(&(p->baggage_quantity), sizeof(int), 1, in);
-    fread(&(p->baggage_weight), sizeof(double), 1, in);
-    fread(p->destination, sizeof(char), SIZE_DESTINATION, in);
-    fread(p->time, sizeof(char), SIZE_TIME, in);
+    fread(&(p->baggage_weight), sizeof(int), 1, in);
+    fread(p->destination, sizeof(char), SIZE_STR, in);
+    fread(p->time, sizeof(char), SIZE_STR, in);
     fread(&(p->transplants), sizeof(char), 1, in);
     fread(&(p->children), sizeof(char), 1, in);
-
+    return !feof(in);
 }
 
 void passenger_print(Passenger *p)

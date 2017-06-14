e6909456ffa7d944a825799295ce29f173a99dff
Denisplusplus <whinslow@mail.ru>
2017-06-14 18:10:34 +0300

    Update passenger.h

--- Code/passenger.h
+++ Code/passenger.h
@@ -1,19 +1,16 @@
 #ifndef __PASSENGER_H__
 #define __PASSENGER_H__
 
-#define SIZE_SURNAME 50
-#define SIZE_INITIALS 50
-#define SIZE_DESTINATION 50
-#define SIZE_TIME 50
+#define SIZE_STR 24
 typedef struct {
-  char surname[SIZE_SURNAME];
-  char initials[SIZE_INITIALS];
-  int baggage_quantity;
-  int baggage_weight;
-  char destination[SIZE_DESTINATION];
-  char time[SIZE_TIME];
-  char transplants;
-  char children;
+	char surname[SIZE_STR];
+	char initials[SIZE_STR];
+	int baggage_quantity;
+	int baggage_weight;
+	char destination[SIZE_STR];
+	char time[SIZE_STR];
+	char transplants;
+	char children;
 } Passenger;
 
 #endif
